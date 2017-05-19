a24495796e2bac478ab60a3e0d62492ee5d9aceb
Rick Danielson <rick.danielson@nersc.no>
2017-05-20 00:22:26 +0200

    symbol transpose fix

--- src/ICOADSDict.jl
+++ src/ICOADSDict.jl
@@ -12,33 +12,33 @@
 module ICOADSDict                                       # first define the values contained in the core
 export imma                                             # and attachments by symbol, location, and scale
 
-const symb0 = [:YR :MO :DY  :HR :LAT :LON :IM :ATTC :TI :LI :DS :VS :NID :II :ID :C1 :DI :D :WI  :W :VI :VV :WW :W1 :SLP :A :PPP :IT :AT :WBTI :WBT :DPTI :DPT :SI :SST :N :NH :CL :HI :H :CM :CH :WD :WP :WH :SD :SP :SH]'
-const leng0 = [  4   2   2    4    5    6   2     1   1   1   1   1    2   2   9   2   1  3   1   3   1   2   2   1    5  1    3   1   4     1    4     1    4   2    4  1   1   1   1  1   1   1   2   2   2   2   2   2]' 
-const scal0 = [  1   1   1 0.01 0.01 0.01   1     1   1   1   1   1    1   1   0   0   1  1   1 0.1   1   1   1   1  0.1  1  0.1   1 0.1     1  0.1     1  0.1   1  0.1  1   1   0   1  0   0   0   1   1   1   1   1   1]'
+const symb0 = [:YR, :MO, :DY,  :HR, :LAT, :LON, :IM, :ATTC, :TI, :LI, :DS, :VS, :NID, :II, :ID, :C1, :DI, :D, :WI,  :W, :VI, :VV, :WW, :W1, :SLP, :A, :PPP, :IT, :AT, :WBTI, :WBT, :DPTI, :DPT, :SI, :SST, :N, :NH, :CL, :HI, :H, :CM, :CH, :WD, :WP, :WH, :SD, :SP, :SH]
+const leng0 = [  4,   2,   2,    4,    5,    6,   2,     1,   1,   1,   1,   1,    2,   2,   9,   2,   1,  3,   1,   3,   1,   2,   2,   1,    5,  1,    3,   1,   4,     1,    4,     1,    4,   2,    4,  1,   1,   1,   1,  1,   1,   1,   2,   2,   2,   2,   2,   2]
+const scal0 = [  1,   1,   1, 0.01, 0.01, 0.01,   1,     1,   1,   1,   1,   1,    1,   1,   0,   0,   1,  1,   1, 0.1,   1,   1,   1,   1,  0.1,  1,  0.1,   1, 0.1,     1,  0.1,     1,  0.1,   1,  0.1,  1,   1,   0,   1,  0,   0,   0,   1,   1,   1,   1,   1,   1]
 
-const symb1 = [:ATTI1 :ATTL1 :BSI :B10 :B1 :DCK :SID :PT :DUPS :DUPC :TC :PB :WX :SX :C2 :SQZ :SQA :AQZ :AQA :UQZ :UQA :VQZ :VQA :PQZ :PQA :DQZ :DQA :ND :SF :AF :UF :VF :PF :RF :ZNC :WNC :BNC :XNC :YNC :PNC :ANC :GNC :DNC :SNC :CNC :ENC :FNC :TNC :QCE :LZ :QCZ]'
-const leng1 = [     2      2    1    3   2    3    3   2     2     1   1   1   1   1   2    1    1    1    1    1    1    1    1    1    1    1    1   1   1   1   1   1   1   1    1    1    1    1    1    1    1    1    1    1    1    1    1    1    2   1    2]'
-const scal1 = [     1      1    0    1   1    1    1   1     1     1   1   1   1   1   1  -36  -36  -36  -36  -36  -36  -36  -36  -36  -36  -36  -36   1 -36 -36 -36 -36 -36 -36  -36  -36  -36  -36  -36  -36  -36  -36  -36  -36  -36  -36  -36  -36    1   1    1]'
+const symb1 = [:ATTI1, :ATTL1, :BSI, :B10, :B1, :DCK, :SID, :PT, :DUPS, :DUPC, :TC, :PB, :WX, :SX, :C2, :SQZ, :SQA, :AQZ, :AQA, :UQZ, :UQA, :VQZ, :VQA, :PQZ, :PQA, :DQZ, :DQA, :ND, :SF, :AF, :UF, :VF, :PF, :RF, :ZNC, :WNC, :BNC, :XNC, :YNC, :PNC, :ANC, :GNC, :DNC, :SNC, :CNC, :ENC, :FNC, :TNC, :QCE, :LZ, :QCZ]
+const leng1 = [     2,      2,    1,    3,   2,    3,    3,   2,     2,     1,   1,   1,   1,   1,   2,    1,    1,    1,    1,    1,    1,    1,    1,    1,    1,    1,    1,   1,   1,   1,   1,   1,   1,   1,    1,    1,    1,    1,    1,    1,    1,    1,    1,    1,    1,    1,    1,    1,    2,   1,    2]
+const scal1 = [     1,      1,    0,    1,   1,    1,    1,   1,     1,     1,   1,   1,   1,   1,   1,  -36,  -36,  -36,  -36,  -36,  -36,  -36,  -36,  -36,  -36,  -36,  -36,   1, -36, -36, -36, -36, -36, -36,  -36,  -36,  -36,  -36,  -36,  -36,  -36,  -36,  -36,  -36,  -36,  -36,  -36,  -36,    1,   1,    1]
 
-const symb2 = [:ATTI2 :ATTL2 :OS :OP :FM :IX :W2 :SGN :SGT :SGH :WMI :SD2 :SP2 :SH2 :IS :ES :RS :IC1 :IC2 :IC3 :IC4 :IC5 :IR :RRR :TR :QCI :QI1 :QI2 :QI3 :QI4 :QI5 :QI6 :QI7 :QI8 :QI9 :QI10 :QI11 :QI12 :QI13 :QI14 :QI15 :QI16 :QI17 :QI18 :QI19 :QI20 :QI21 :HDG :COG :SOG :SLL :SLHH :RWD :RWS]'
-const leng2 = [     2      2   1   1   2   1   1    1    1    2    1    2    2    2   1   2   1    1    1    1    1    1   1    3   1    1    1    1    1    1    1    1    1    1    1     1     1     1     1     1     1     1     1     1     1     1     1    3    3    2    2     3    3    3]'
-const scal2 = [     1      1   1   1   1   1   1    1    0    1    1    1    1    1   1   1   1    0    0    0    0    0   1    1   1    1    1    1    1    1    1    1    1    1    1     1     1     1     1     1     1     1     1     1     1     1     1    1    1    1    1     1    1    1]'
+const symb2 = [:ATTI2, :ATTL2, :OS, :OP, :FM, :IX, :W2, :SGN, :SGT, :SGH, :WMI, :SD2, :SP2, :SH2, :IS, :ES, :RS, :IC1, :IC2, :IC3, :IC4, :IC5, :IR, :RRR, :TR, :QCI, :QI1, :QI2, :QI3, :QI4, :QI5, :QI6, :QI7, :QI8, :QI9, :QI10, :QI11, :QI12, :QI13, :QI14, :QI15, :QI16, :QI17, :QI18, :QI19, :QI20, :QI21, :HDG, :COG, :SOG, :SLL, :SLHH, :RWD, :RWS]
+const leng2 = [     2,      2,   1,   1,   2,   1,   1,    1,    1,    2,    1,    2,    2,    2,   1,   2,   1,    1,    1,    1,    1,    1,   1,    3,   1,    1,    1,    1,    1,    1,    1,    1,    1,    1,    1,     1,     1,     1,     1,     1,     1,     1,     1,     1,     1,     1,     1,    3,    3,    2,    2,     3,    3,    3]
+const scal2 = [     1,      1,   1,   1,   1,   1,   1,    1,    0,    1,    1,    1,    1,    1,   1,   1,   1,    0,    0,    0,    0,    0,   1,    1,   1,    1,    1,    1,    1,    1,    1,    1,    1,    1,    1,     1,     1,     1,     1,     1,     1,     1,     1,     1,     1,     1,     1,    1,    1,    1,    1,     1,    1,    1]
 
-const symb3 = [:ATTI3 :ATTL3 :CCCC :BUID :BMP :BSWU :SWU :BSWV :SWV :BSAT :BSRH :SRH :SIX :BSST :MST :MSH :BY :BM :BD :BH :BFL]'
-const leng3 = [     2      2     4     6    5     4    4     4    4     4     3    3    1     4    1    3   4   2   2   2    2]'
-const scal3 = [     1      1     0     0  0.1   0.1  0.1   0.1  0.1   0.1     1    1    1   0.1    1    1   1   1   1   1    1]'
+const symb3 = [:ATTI3, :ATTL3, :CCCC, :BUID, :BMP, :BSWU, :SWU, :BSWV, :SWV, :BSAT, :BSRH, :SRH, :SIX, :BSST, :MST, :MSH, :BY, :BM, :BD, :BH, :BFL]
+const leng3 = [     2,      2,     4,     6,    5,     4,    4,     4,    4,     4,     3,    3,    1,     4,    1,    3,   4,   2,   2,   2,    2]
+const scal3 = [     1,      1,     0,     0,  0.1,   0.1,  0.1,   0.1,  0.1,   0.1,     1,    1,    1,   0.1,    1,    1,   1,   1,   1,   1,    1]
 
-const symb4 = [:ATTI4 :ATTL4 :C1M :OPM :KOV :COR :TOB :TOT :EOT :LOT :TOH :EOH :SIM :LOV :DOS :HOP :HOT :HOB :HOA :SMF :SME :SMV]'
-const leng4 = [     2      2    2    2    2    2    3    3    2    2    1    2    3    3    2    3    3    3    3    5    5    2]'
-const scal4 = [     1      1    0    1    0    0    0    0    0    0    0    0    0    1    1    1    1    1    1    1    1    1]'
+const symb4 = [:ATTI4, :ATTL4, :C1M, :OPM, :KOV, :COR, :TOB, :TOT, :EOT, :LOT, :TOH, :EOH, :SIM, :LOV, :DOS, :HOP, :HOT, :HOB, :HOA, :SMF, :SME, :SMV]
+const leng4 = [     2,      2,    2,    2,    2,    2,    3,    3,    2,    2,    1,    2,    3,    3,    2,    3,    3,    3,    3,    5,    5,    2]
+const scal4 = [     1,      1,    0,    1,    0,    0,    0,    0,    0,    0,    0,    0,    0,    1,    1,    1,    1,    1,    1,    1,    1,    1]
 
-const symb5 = [:ATTI5 :ATTL5 :WFI :WF :XWI :XW :XDI :XD :SLPI :TAI :TA :XNI :XN :OTHERSTUFF5]'
-const leng5 = [     2      2    1   2    1   3    1   2     1    1   4    1   2           71]'
-const scal5 = [     1      1    0   1    0   1    0   0     0    0   1    0   0            0]'
+const symb5 = [:ATTI5, :ATTL5, :WFI, :WF, :XWI, :XW, :XDI, :XD, :SLPI, :TAI, :TA, :XNI, :XN, :OTHERSTUFF5]
+const leng5 = [     2,      2,    1,   2,    1,   3,    1,   2,     1,    1,   4,    1,   2,           71]
+const scal5 = [     1,      1,    0,   1,    0,   1,    0,   0,     0,    0,   1,    0,   0,            0]
 
-const symb6 = [:ATTI6 :ATTL6 :ATTE :OTHERSTUFF6]'
-const leng6 = [     2      2     1          999]'
-const scal6 = [     1      1     0            0]'
+const symb6 = [:ATTI6, :ATTL6, :ATTE, :OTHERSTUFF6]
+const leng6 = [     2,      2,     1,          999]
+const scal6 = [     1,      1,     0,            0]
 
 function imma(line::AbstractString)
   vals = Dict{Symbol, Any}()                                                  # initialize a "vals" dictionary
